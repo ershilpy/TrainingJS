dab05fe0bb5d61753ccc1e552a5e1e7aee7c3abb
ershilpy <ershilpygupta@gmail.com>
2023-05-12 09:55:26 +0530

    wecome

---
+++ Session-1/FirstProg.js
@@ -0,0 +1,10 @@
+console.log("This is first day of training")
+
+for(var i=0; i<10; i++)
+{
+    console.log(i)
+}
+function sayhello(name){
+    return "hello" + name
+}
+console.log(sayhello("GU"))
